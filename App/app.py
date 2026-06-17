ce9e2aa99fbbb6b174310a2a505a66d12d9da4f2
Dale Faubion <dalefaubion@gmail.com>
2026-06-17 13:53:44 -0700

    Updated: Updated models to be compatible with render hopefully

--- App/app.py
+++ App/app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 # Load models once at startup
 models = [
-    load_model("App/models/Best_model.keras"),
-    load_model("App/models/Best_second_model.keras"),
-    load_model("App/models/Best_third_model.keras")
+    load_model("App/models/Best_model_c.keras"),
+    load_model("App/models/Best_second_model_c.keras"),
+    load_model("App/models/Best_third_model_c.keras")
 ]
 
 # CIFAR-10 labels
